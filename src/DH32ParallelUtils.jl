b7e8eda82eb4871e39a4cbb52fd12dc70c871787
Daniel Hoffmann <daniel.hoffmann@uni-due.de>
2017-01-22 01:38:46 +0100

    first commit

--- src/DH32ParallelUtils.jl
+++ src/DH32ParallelUtils.jl
@@ -1,5 +1,34 @@
 module DH32ParallelUtils
 
 # package code goes here
+export split_int
+
+"""
+Splits an integer number N into a set of n_procs contiguous, non-overlapping integer intervals [ix_start[i], ix_end[i]].
+
+Input:
+
+- N: number to be split.
+
+- n_procs: number of processes.
+
+- delta (default: 1.0e-20): number added for application of floor and ceil.
+
+Output: ix_start, ix_end
+
+- ix_start: int array of start indexes.
+
+- ix_end: int array of end indexes.
+"""
+function split_int(N::Int64, n_procs::Int64, delta::Float64=1.0e-20)
+    ix_start = zeros(Int64,n_procs)
+    ix_end = zeros(Int64,n_procs)
+    float_bounds = linspace(delta, Float64(N)+delta, n_procs+1)
+    for i in 1:n_procs
+        ix_start[i] = ceil(Int64,float_bounds[i])
+        ix_end[i] = floor(Int64,float_bounds[i+1])
+    end
+    ix_start, ix_end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,6 @@ using DH32ParallelUtils
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+ix_start, ix_end = split_int(2000, 3)
+@test ix_start == [1,667,1334]
+@test ix_end == [666,1333,2000]
