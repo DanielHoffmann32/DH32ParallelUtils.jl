60543d497ebcb1c23f97024a3d5e06a7ce951871
Daniel Hoffmann <daniel.hoffmann@uni-due.de>
2017-01-22 11:30:08 +0100

    debugged split_int, updated tests

--- src/DH32ParallelUtils.jl
+++ src/DH32ParallelUtils.jl
@@ -12,21 +12,21 @@ Input:
 
 - n_procs: number of processes.
 
-- delta (default: 1.0e-20): number added for application of floor and ceil.
-
 Output: ix_start, ix_end
 
 - ix_start: int array of start indexes.
 
 - ix_end: int array of end indexes.
 """
-function split_int(N::Int64, n_procs::Int64, delta::Float64=1.0e-20)
+function split_int(N::Int64, n_procs::Int64)
     ix_start = zeros(Int64,n_procs)
     ix_end = zeros(Int64,n_procs)
-    float_bounds = linspace(delta, Float64(N)+delta, n_procs+1)
-    for i in 1:n_procs
-        ix_start[i] = ceil(Int64,float_bounds[i])
-        ix_end[i] = floor(Int64,float_bounds[i+1])
+    float_bounds = linspace(1., Float64(N), n_procs+1)
+    ix_start[1] = 1
+    ix_end[n_procs] = N
+    for i in 2:n_procs
+        ix_start[i] = round(Int64,float_bounds[i])
+        ix_end[i-1] = ix_start[i]-1
     end
     ix_start, ix_end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@ using DH32ParallelUtils
 using Base.Test
 
 # write your own tests here
-ix_start, ix_end = split_int(2000, 3)
+
+### split_int ###
+ix_start, ix_end = DH32ParallelUtils.split_int(2000, 3)
 @test ix_start == [1,667,1334]
 @test ix_end == [666,1333,2000]
+
+ix_start, ix_end = DH32ParallelUtils.split_int(2000, 2)
+@test ix_start == [1,1000]
+@test ix_end == [999,2000]
